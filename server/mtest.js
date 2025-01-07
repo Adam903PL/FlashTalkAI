39334c9e5f77c1611d13de9015c50dbe86732fca
Adam Pukaluk <pukaluk.adam505@gmail.com>
2025-01-07 19:48:44 +0100

    version 1.19 test folder with json

--- server/mtest.js
+++ server/mtest.js
@@ -1,23 +1,21 @@
-fetch("http://localhost:4444/getuserpoint")
-.then((resp) => resp.json())
-.then((data) => {
-  console.log("Selected data from database:", data.data);
-});
+
+
+const path = require("path");
+
+
+const fs = require('fs');
+
+
+function getTestData(directory = './test') {
+  try {
+      const files = fs.readdirSync(directory);
+      return files.filter(file => fs.statSync(path.join(directory, file)).isFile());
+  } catch (err) {
+      console.error(`Error reading directory: ${err}`);
+      return [];
+  }
+}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(getTestData())
