f72d14cc6e0ffb93517b1556b261f53d3b7eb68d
OskarKurzyna <jan.kowalski@example.com>
2025-01-15 22:24:52 +0100

    wyświetlam formularz, ale jest problem z inputami

--- src/pages/tests/TestPage.tsx
+++ src/pages/tests/TestPage.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 export const TestPage = ({unit}:any) => {
-  const { unitId } = useParams<{ unitId: string }>(); // Zmieniłem na te useParamsy, pobieramy id z URL
   const [questions, setQuestions] = useState<
                   // Tutaj znowu ten taki "szablon" do pytań
     { id: number; question: string; type: "fillthegap" | "simpletranslation"; answer: string }[]
@@ -14,8 +13,8 @@ export const TestPage = ({unit}:any) => {
 
   // Ładowanie pytań na podstawie unitId
   useEffect(() => {
-    if (unitId) {
-      fetch(`/api/questions/${unitId}.json`) // Uniersalna ścieżka do backendu
+    if (unit) {
+      fetch(`http://localhost:4444/api/test/${unit}`) // Uniersalna ścieżka do backendu
         .then((res) => res.json())
         .then((data) => {
           // Łączymy pytania z różnych typów zadań (w razie czego to wam wyjaśnię w szkole bo już mi się nie chce komentować)
@@ -27,7 +26,7 @@ export const TestPage = ({unit}:any) => {
         })
         .catch((error) => console.error("Błąd podczas pobierania pytań:", error));
     }
-  }, [unitId]);
+  }, [unit]);
 
   // Obsługa wpisywania odpowiedzi
   const handleChange = (id: number, value: string) => {
@@ -51,7 +50,7 @@ export const TestPage = ({unit}:any) => {
 
   return (
     <div>
-      <h1>Test: {unitId}</h1>
+      <h1>Test: {unit}</h1>
       {!isSubmitted ? (
         <form onSubmit={handleSubmit}>
           {questions.map((question) => (
@@ -59,28 +58,28 @@ export const TestPage = ({unit}:any) => {
               <h2>
                 {question.type === "fillthegap" ? (
                   <>
-                    Uzupełnij lukę: <span>{question.question}</span>
+                    Fill the gap: <span>{question.question}</span>
                   </>
                 ) : (
                   <>
-                    Przetłumacz: <span>{question.question}</span>
+                    Translate: <span>{question.question}</span>
                   </>
                 )}
               </h2>
               <input
                 type="text"
-                placeholder="Twoja odpowiedź"
+                placeholder="Your answer:"
                 value={userAnswers[question.id] || ""}
                 onChange={(e) => handleChange(question.id, e.target.value)}
               />
             </div>
           ))}
-          <button type="submit">Zakończ test</button>
+          <button type="submit">Submit Your Answers</button>
         </form>
       ) : (
         <div>
           <h2>
-            Twój wynik: {score} / {questions.length}
+            Your score: {score} / {questions.length}
           </h2>
         </div>
       )}
