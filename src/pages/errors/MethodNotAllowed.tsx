1b7793c2e21ee3990fb36c1d6c4fdb47f26aec45
Adam Pukaluk <pukaluk.adam505@gmail.com>
2025-01-17 22:49:58 +0100

    taliwnd

---
+++ src/pages/errors/MethodNotAllowed.tsx
@@ -0,0 +1,20 @@
+// import React from 'react';
+// import Lottie from 'lottie-react';
+// import NavBar from '../navbar';
+
+// const MethodNotAllowed = () => {
+//   const style = { height: 400 };
+
+//   return (
+//     <div className="bg-gradient-to-br from-green-500 to-black min-h-screen flex flex-col items-center justify-center text-white">
+//       <NavBar />
+//       <div className="flex flex-col items-center justify-center">
+//         <Lottie animationData={methodNotAllowedAnimation} style={style} loop autoplay />
+//         <h2 className="text-3xl font-bold mb-4">405 - Metoda Niedozwolona</h2>
+//         <p className="text-lg">Przepraszamy, ta metoda nie jest dozwolona dla tej ścieżki.</p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default MethodNotAllowed;
--- src/pages/tests/TestListPage.tsx
+++ src/pages/tests/TestListPage.tsx
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from "react";
 import NavBar from "../navbar";
 import { UnitSelctComponent } from "./UnitsSelectComponent";
-// import "../css/TestListPage.css"; 
+import "../css/TestListPage.css"; 
 import { useNavigate } from "react-router-dom";
 import { useRef } from "react";
 import LottieView, { type LottieRefCurrentProps } from "lottie-react";
 import animationJson from "../../assets/Loading.json";
+
 export const TestListPage = () => {
   const [testUnits, setTestUnits] = useState<string[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const lottieRef = useRef<LottieRefCurrentProps | null>(null);
 
   useEffect(() => {
@@ -18,31 +19,38 @@ export const TestListPage = () => {
       .then((data) => {
         setLoading(false);
         setTestUnits(data);
-
-        
       })
       .catch((err) => {
-        console.log("Error during fetch:",err);
+        console.log("Error during fetch:", err);
       });
   }, []);
 
-    if (loading) {
-        return (
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-                <LottieView animationData={animationJson} lottieRef={lottieRef} />
-            </div>
-        );
-    }
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen bg-gradient-to-r from-gray-800 to-black">
+        <LottieView animationData={animationJson} lottieRef={lottieRef} />
+      </div>
+    );
+  }
 
   return (
     <>
       <NavBar />
-      <div className="unit-container">
-        {testUnits.map((unit, index) => (
-          <div key={index} className="unit-item" onClick={()=>{const unitName = unit.split("Test.json")[0];navigate(`/home/test/${unitName}`)}}>
-            <UnitSelctComponent unit={unit} />
-          </div>
-        ))}
+      <div className="bg-gradient-to-r from-gray-800 to-black min-h-screen text-white py-10">
+        <div className="unit-container">
+          {testUnits.map((unit, index) => (
+            <div
+              key={index}
+              className="unit-item"
+              onClick={() => {
+                const unitName = unit.split("Test.json")[0];
+                navigate(`/home/test/${unitName}`);
+              }}
+            >
+              <UnitSelctComponent unit={unit} />
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
