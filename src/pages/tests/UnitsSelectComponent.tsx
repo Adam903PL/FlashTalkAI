f2319272248cb429a784dba9f01630580af5956c
Adam Pukaluk <pukaluk.adam505@gmail.com>
2025-01-18 17:57:15 +0100

    1.22.2

--- src/pages/tests/UnitsSelectComponent.tsx
+++ src/pages/tests/UnitsSelectComponent.tsx
@@ -1,9 +1,9 @@
-// import "../css/TestListPage.css"; // Import the styles
-
 export const UnitSelctComponent = ({ unit }: { unit: string }) => {
   return (
     <>
-      <h1>{unit.split("Test.json")[0]}</h1>
+      <h1 className="text-2xl font-bold text-cyan-400 text-center mb-2.5">
+        {unit.split("Test.json")[0]}
+      </h1>
       <p className="text-sm text-gray-300 text-center mt-2 mb-2 max-w-[400px] mx-auto p-2 bg-[#2f3134] rounded-lg leading-6">
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis facere
         natus tempora corrupti beatae! Iure omnis molestias nisi autem
