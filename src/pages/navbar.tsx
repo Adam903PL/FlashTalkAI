e4f379facdcaea9b191ba6db78e33b7fb6318dfc
Konrad Klautzsch <konradklautzsch@gmail.com>
2025-01-19 11:38:11 +0100

    lekkie zmiany w nav bar

--- src/pages/navbar.tsx
+++ src/pages/navbar.tsx
@@ -1,85 +1,3 @@
-// import { useEffect, useState } from "react";
-// import { useNavigate } from "react-router-dom";
-// import "./css/headerNav.css";
-// import { useLoged } from "../contexts/loged/useLoged";
-//
-// function NavBar() {
-//   const [userMenuVisible, setUserMenuVisible] = useState(false);
-//   const [navMenuVisible, setNavMenuVisible] = useState(false);
-//   const { loged, setloged } = useLoged();
-//   const navigate = useNavigate();
-//
-//   const [isExpanded, setIsExpanded] = useState(false);
-//
-//   const toggleExpand = () => {
-//     setIsExpanded(!isExpanded);
-//   };
-//
-//   const toggleUserMenu = () => setUserMenuVisible(!userMenuVisible);
-//   const toggleNavMenu = () => setNavMenuVisible(!navMenuVisible);
-//   const logout = () => {
-//     fetch("http://localhost:4444/logout", { credentials: "include" })
-//       .catch((err) => {console.error("Errors during logout:", err)});
-//       setloged(false);
-//
-//       navigate("/login");
-//   };
-//
-//   return (
-//     <>
-//       {/* Header */}
-//       <header className="header">
-//         <div className="logo" onClick={() => navigate("/home")}>
-//           <h1>FlashTalkAI</h1>
-//         </div>
-//
-//         <div className="searchContainer">
-//           <input
-//             type="text"
-//             placeholder="Wyszukaj..."
-//             className="searchInput"
-//           />
-//         </div>
-//         <div className="menuToggle" onClick={toggleNavMenu}>
-//           <i className={`fas fa-bars "hamburgerIcon"`}></i>
-//         </div>
-//
-//         <div className="userContainer" onClick={toggleUserMenu}>
-//           <h1 className={`Options`} onClick={toggleExpand}>Options ⚙️</h1>
-//           {isExpanded && (
-//           <div className="userMenu">
-//             <ul>
-//               <li onClick={() => navigate("/settings")}>Ustawienia</li>
-//               <li
-//                 onClick={logout}
-//               >
-//                 Wyloguj się
-//               </li>
-//               <li onClick={() => navigate("/options")}>Opcje strony</li>
-//             </ul>
-//           </div>
-//               )}
-//         </div>
-//       </header>
-//
-//       {/* Navigation Menu */}
-//       {(navMenuVisible || window.innerWidth > 768) && (
-//         <nav className="navMenu">
-//           <ul>
-//             <li onClick={() => navigate("/home/learn")}>Ucz się AI</li>
-//             <li onClick={() => navigate("/home/voice-practice")}>
-//               Praktyka Głosowa
-//             </li>
-//             <li onClick={() => navigate("/home/flashcards")}>Fiszki</li>
-//             <li onClick={() => navigate("/home/test")}>Test</li>
-//           </ul>
-//         </nav>
-//       )}
-// </>
-//   );
-// }
-//
-// export default NavBar;
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./css/headerNav.css";
@@ -120,7 +38,7 @@ function NavBar() {
             />
           </div>
           <h1 className="navToggleBtn" onClick={toggleNavMenu}>
-            asdadsads
+            asdadas
           </h1>
           <div className="userContainer" onClick={toggleUserMenu}>
             <h1 className="Options">Options ⚙️</h1>
