562a5db78d8c37fe485b5918a0025743c0b6e686
Adam Pukaluk <pukaluk.adam505@gmail.com>
2025-01-20 20:29:40 +0100

    2.2.0 bez 2 node modulsów naprawione że jełśi user nie wgrął zdj ustawi amu się defultowe

--- src/pages/NavBars/navbar.tsx
+++ src/pages/NavBars/navbar.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useLoged } from "../../contexts/loged/useLoged";
 import { useSpring, animated } from "react-spring"; // Importujemy react-spring
-import useUserId from "../../zustand/useUserId";
+
 
 type getData = {
   succes: boolean;
